acd5903c6eabaddea9905b6a39436344ea44f3a3
aanper <mail@s3f.ru>
2021-03-13 23:22:28 +0300

    add planes and control each

--- app.js
+++ app.js
@@ -9,27 +9,6 @@ const camera = new THREE.PerspectiveCamera(
     75, window.innerWidth / window.innerHeight, 0.1, 1000
 );
 
-const vertex_shader = vert`
-    varying vec2 _uv;
-
-    void main() {
-        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-        _uv = uv;
-    }
-`;
-
-const fragment_shader = frag`
-    precision mediump float;
-    precision mediump int;
-
-    varying vec2 _uv;
-
-    void main() {
-        gl_FragColor = vec4(vec3(pow(_uv.x - 0.5, 2.) * 2. + pow(_uv.y - 0.5, 2.) * 2., 0., 0.), 1.);
-    }
-`;
-
-const geometry = new THREE.PlaneGeometry(1, 1);
 /*
 const geometry = new THREE.BufferGeometry();
 const vertices = new Float32Array([
@@ -46,37 +25,55 @@ geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
 geometry.setAttribute('uv', new THREE.BufferAttribute(uv, 2));
 */
 
-console.log(vertex_shader, fragment_shader);
-
-const material = new THREE.ShaderMaterial( {
-	uniforms: {
-		time: { value: 1.0 }
-    },
-    vertexShader: vertex_shader[0],
-	fragmentShader: fragment_shader[0],
-} );
-
-const plane = new THREE.Mesh(geometry, material);
-
 function animate() {
 	requestAnimationFrame(animate);
 	renderer.render(scene, camera);
 }
 
-function app() {
-    const gui = new dat.GUI();
+function add_plane(scene, folder) {
+    const geometry = new THREE.PlaneGeometry(1, 1);
 
-    let folder = gui.addFolder("1");
+    const vertex_shader = vert`
+        varying vec2 _uv;
 
+        void main() {
+            gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+            _uv = uv;
+        }
+    `;
+
+    const fragment_shader = frag`
+        precision mediump float;
+        precision mediump int;
+
+        varying vec2 _uv;
+
+        void main() {
+            gl_FragColor = vec4(vec3(pow(_uv.x - 0.5, 2.) * 2. + pow(_uv.y - 0.5, 2.) * 2., 0., 0.), 1.);
+        }
+    `;
+
+    const material = new THREE.ShaderMaterial( {
+        uniforms: {
+            time: { value: 1.0 }
+        },
+        vertexShader: vertex_shader[0],
+        fragmentShader: fragment_shader[0],
+    } );
+    
+    const plane = new THREE.Mesh(geometry, material);
+
+    scene.add(plane);
+
+    
     let param = {
-        name: 'Sam',
-        add_plane: () => {console.log("add plane")},
         rotate_x: 0,
         rotate_y: 0,
         rotate_z: 0,
         x: 0,
         y: 0,
     };
+
     folder.add(param, 'rotate_x')
         .min(-1.5).max(1.5).step(.01)
         .listen().onChange(value => plane.rotation.x = value);
@@ -93,13 +90,26 @@ function app() {
     folder.add(param, 'y')
         .min(-5).max(5).step(.05)
         .listen().onChange(value => plane.position.y = value);
-    
+}
+
+function app() {
+    const gui = new dat.GUI();
+
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    
-    scene.add(plane);
-    
+    let plane_id = 0;
+
+    let param = {
+        add_plane: () => {
+            let folder = gui.addFolder("plane" + plane_id);
+            plane_id++;
+            add_plane(scene, folder);
+        }
+    };
+
+    gui.add(param, "add_plane");
+
     camera.position.z = 5;
 
     animate();
